8f39afe7d245770996bb5f435e309b39752dbc68
Jennifer Peavler <jenn.peavler@gmail.com>
2017-08-21 14:37:32 -0600

    completed hero box.  next step, more custom styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <header>
+      <h1>Welcome to {{title}}</h1>
+    </header>
+    <nav>
+    <a routerLink='/dashboard' routerLinkActive='active'>Dashboard</a>
+    <a routerLink='/heroes' routerLinkActive='active'>Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  title = 'Hero box';
+}
